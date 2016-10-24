1cbc931a647382b6531b44758980c84ca326dd0f
rogerpaviani <rogerpaviani@gmail.com>
2016-10-23 23:03:41 -0200

    css currency; removido codigo obsoleto;

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,14 @@
     card: d.querySelector('.card.balance'),
     update: function (v) {
       idbKeyval.set('balance', v)
-        .then(() => balance.card.querySelector('.stitle').textContent = v.toLocaleString('pt-br'))
+        .then(() => balance.card.querySelector('.currency').textContent = v.toLocaleString('pt-br'))
         .catch(err => console.log('balance.update failed!', err));
     },
     show: function () {
       this.card.removeAttribute('hidden');
     },
     hide: function () {
-      this.card.removeAttribute('hidden');
+      this.card.setAttribute('hidden', true);
     },
   };
 
@@ -29,7 +29,7 @@
       this.card.removeAttribute('hidden');
     },
     hide: function () {
-      this.card.removeAttribute('hidden');
+      this.card.setAttribute('hidden', true);
     },
     update: function () {
       var c = this.container;
@@ -77,24 +77,16 @@
   };
 
   ////////////////////////////////////////////////////////////////////////////////////
-  var app = {
+  const app = {
     hasRequestPending: false,
     isLoading: true,
-    visibleCards: {},
-    selectedCities: [],
     spinner: document.querySelector('.loader'),
     cardTemplate: document.querySelector('.cardTemplate'),
     container: document.querySelector('.main'),
     addDialog: document.querySelector('.dialog-container'),
     menuDialog: document.querySelector('.menu-container'),
-    daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   };
 
-  /*****************************************************************************
-   *
-   * Methods to update/refresh the UI
-   *
-   ****************************************************************************/
   app.hideAllCards = function () {
     app.container.querySelectorAll('.card').forEach(function (elem) {
       elem.setAttribute('hidden', true);
@@ -109,14 +101,13 @@
     }
   }
 
-  // Toggles the visibility of the add new city dialog.
-  app.toggleAddDialog = function (visible) {
-    if (visible) {
-      app.addDialog.classList.add('dialog-container--visible');
-    } else {
-      app.addDialog.classList.remove('dialog-container--visible');
-    }
-  };
+  // app.toggleAddDialog = function (visible) {
+  //   if (visible) {
+  //     app.addDialog.classList.add('dialog-container--visible');
+  //   } else {
+  //     app.addDialog.classList.remove('dialog-container--visible');
+  //   }
+  // };
 
   app.init = function () {
     balance.update(52678.90);
@@ -137,89 +128,6 @@
     }
   };
 
-
-  /*****************************************************************************
-   *
-   * Methods for dealing with the model
-   *
-   ****************************************************************************/
-
-  // Gets a forecast for a specific city and update the card with the data
-  app.getForecast = function (key, label) {
-    var url = 'https://publicdata-weather.firebaseio.com/';
-    url += key + '.json';
-    if ('caches' in window) {
-      caches.match(url).then(function (response) {
-        if (response) {
-          response.json().then(function (json) {
-            // Only update if the XHR is still pending, otherwise the XHR
-            // has already returned and provided the latest data.
-            if (app.hasRequestPending) {
-              console.log('updated from cache');
-              json.key = key;
-              json.label = label;
-              app.updateForecastCard(json);
-            }
-          });
-        }
-      });
-    }
-    // Make the XHR to get the data, then update the card
-    app.hasRequestPending = true;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-          var response = JSON.parse(request.response);
-          response.key = key;
-          response.label = label;
-          app.hasRequestPending = false;
-          app.updateForecastCard(response);
-        }
-      }
-    };
-    request.open('GET', url);
-    request.send();
-  };
-
-  // Iterate all of the cards and attempt to get the latest forecast data
-  app.updateForecasts = function () {
-    var keys = Object.keys(app.visibleCards);
-    keys.forEach(function (key) {
-      app.getForecast(key);
-    });
-  };
-
-  // Save list of cities to localStorage, see note below about localStorage.
-  app.saveSelectedCities = function () {
-    var selectedCities = JSON.stringify(app.selectedCities);
-    // IMPORTANT: See notes about use of localStorage.
-    localStorage.selectedCities = selectedCities;
-  };
-
-  // app.selectedCities = localStorage.selectedCities;
-  // if (app.selectedCities) {
-  //   app.selectedCities = JSON.parse(app.selectedCities);
-  //   app.selectedCities.forEach(function (city) {
-  //     app.getForecast(city.key, city.label);
-  //   });
-  // } else {
-  //   app.updateForecastCard(initialWeatherForecast);
-  //   app.selectedCities = [{
-  //     key: initialWeatherForecast.key,
-  //     label: initialWeatherForecast.label
-  //   }];
-  //   app.saveSelectedCities();
-  // }
-
-  // if('serviceWorker' in navigator) {
-  //   navigator.serviceWorker
-  //            .register('./service-worker.js')
-  //            .then(function() { console.log('Service Worker Registered'); });
-  // }
-
-  app.init();
-
   (window.onhashchange = function () {
     switch (l.hash) {
       case "#menu":
@@ -237,5 +145,101 @@
         }
     }
   })();
+
+  app.init();
+
+  // if('serviceWorker' in navigator) {
+  //   navigator.serviceWorker
+  //            .register('./service-worker.js')
+  //            .then(function() { console.log('Service Worker Registered'); });
+  // }
+
+  // FIM FIM FIM FIM FIM FIM FIM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // // Gets a forecast for a specific city and update the card with the data
+  // app.getForecast = function (key, label) {
+  //   var url = 'https://publicdata-weather.firebaseio.com/';
+  //   url += key + '.json';
+  //   if ('caches' in window) {
+  //     caches.match(url).then(function (response) {
+  //       if (response) {
+  //         response.json().then(function (json) {
+  //           // Only update if the XHR is still pending, otherwise the XHR
+  //           // has already returned and provided the latest data.
+  //           if (app.hasRequestPending) {
+  //             console.log('updated from cache');
+  //             json.key = key;
+  //             json.label = label;
+  //             app.updateForecastCard(json);
+  //           }
+  //         });
+  //       }
+  //     });
+  //   }
+  //   // Make the XHR to get the data, then update the card
+  //   app.hasRequestPending = true;
+  //   var request = new XMLHttpRequest();
+  //   request.onreadystatechange = function () {
+  //     if (request.readyState === XMLHttpRequest.DONE) {
+  //       if (request.status === 200) {
+  //         var response = JSON.parse(request.response);
+  //         response.key = key;
+  //         response.label = label;
+  //         app.hasRequestPending = false;
+  //         app.updateForecastCard(response);
+  //       }
+  //     }
+  //   };
+  //   request.open('GET', url);
+  //   request.send();
+  // };
+
+  // // Iterate all of the cards and attempt to get the latest forecast data
+  // app.updateForecasts = function () {
+  //   var keys = Object.keys(app.visibleCards);
+  //   keys.forEach(function (key) {
+  //     app.getForecast(key);
+  //   });
+  // };
+
+  // // Save list of cities to localStorage, see note below about localStorage.
+  // app.saveSelectedCities = function () {
+  //   var selectedCities = JSON.stringify(app.selectedCities);
+  //   // IMPORTANT: See notes about use of localStorage.
+  //   localStorage.selectedCities = selectedCities;
+  // };
+
+  // app.selectedCities = localStorage.selectedCities;
+  // if (app.selectedCities) {
+  //   app.selectedCities = JSON.parse(app.selectedCities);
+  //   app.selectedCities.forEach(function (city) {
+  //     app.getForecast(city.key, city.label);
+  //   });
+  // } else {
+  //   app.updateForecastCard(initialWeatherForecast);
+  //   app.selectedCities = [{
+  //     key: initialWeatherForecast.key,
+  //     label: initialWeatherForecast.label
+  //   }];
+  //   app.saveSelectedCities();
+  // }
 
 })(document, location, idbKeyval);
