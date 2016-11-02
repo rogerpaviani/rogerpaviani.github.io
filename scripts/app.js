9187e02d783eaba7938d8b6a72ec2c6f8f46d449
rogerpaviani <rogerpaviani@gmail.com>
2016-11-02 17:50:35 -0200

    cards viraram sections

--- scripts/app.js
+++ scripts/app.js
@@ -7,17 +7,17 @@
 
   ////////////////////////////////////////////////////////////////////////////////////
   const balance = {
-    card: d.querySelector('.card.balance'),
+    section: d.querySelector('section.balance'),
     update: function (v) {
       idbKeyval.set('balance', v)
-        .then(() => balance.card.querySelector('.currency').textContent = v.toLocaleString('pt-br'))
+        .then(() => balance.section.querySelector('.currency').textContent = v.toLocaleString('pt-br'))
         .catch(err => console.log('balance.update failed!', err));
     },
     show: function () {
-      this.card.removeAttribute('hidden');
+      this.section.removeAttribute('hidden');
     },
     hide: function () {
-      this.card.setAttribute('hidden', true);
+      this.section.setAttribute('hidden', true);
     },
     init: function () {
       balance.update(52678.90);
@@ -30,14 +30,14 @@
     p: new Promise(function (resolve) {
       resolve()
     }),
-    card: d.querySelector('.card.income'),
-    template: d.querySelector('.card.income .entryTemplate'),
-    container: d.querySelector('.card.income .entries'),
+    section: d.querySelector('section.income'),
+    template: d.querySelector('section.income .entryTemplate'),
+    container: d.querySelector('section.income .entries'),
     show: function () {
-      this.card.removeAttribute('hidden');
+      this.section.removeAttribute('hidden');
     },
     hide: function () {
-      this.card.setAttribute('hidden', true);
+      this.section.setAttribute('hidden', true);
     },
     update: function () {
       var c = this.container;
@@ -45,7 +45,7 @@
         while (c.lastChild) {
           c.removeChild(c.lastChild);
         }!!ii && ii.forEach(function (i, n) {
-          //escrever no card
+          //escrever no section
           //{ val: 99.8, desc: 'jantar', date: 20161001 }
           var entry = income.template.cloneNode(true);
           entry.classList.remove('entryTemplate');
@@ -98,13 +98,12 @@
     hasRequestPending: false,
     isLoading: true,
     spinner: document.querySelector('.loader'),
-    cardTemplate: document.querySelector('.cardTemplate'),
     container: document.querySelector('main'),
     addDialog: document.querySelector('.dialog-container'),
   };
 
-  app.hideAllCards = function () {
-    app.container.querySelectorAll('.card').forEach(function (elem) {
+  app.hideAllSections = function () {
+    app.container.querySelectorAll('section').forEach(function (elem) {
       elem.setAttribute('hidden', true);
     });
   };
@@ -140,9 +139,9 @@
   (window.onhashchange = function () {
     switch (l.hash) {
       default: {
-        app.hideAllCards();
+        app.hideAllSections();
         app.toggleMenu(false);
-        app.container.querySelectorAll('.card.' + (l.hash.substr(1) || 'default')).forEach(function (elem) {
+        app.container.querySelectorAll('section.' + (l.hash.substr(1) || 'default')).forEach(function (elem) {
           elem.removeAttribute('hidden');
         });
       }
@@ -155,94 +154,6 @@
   //   navigator.serviceWorker
   //            .register('./service-worker.js')
   //            .then(function() { console.log('Service Worker Registered'); });
-  // }
-
-  // FIM FIM FIM FIM FIM FIM FIM
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // // Gets a forecast for a specific city and update the card with the data
-  // app.getForecast = function (key, label) {
-  //   var url = 'https://publicdata-weather.firebaseio.com/';
-  //   url += key + '.json';
-  //   if ('caches' in window) {
-  //     caches.match(url).then(function (response) {
-  //       if (response) {
-  //         response.json().then(function (json) {
-  //           // Only update if the XHR is still pending, otherwise the XHR
-  //           // has already returned and provided the latest data.
-  //           if (app.hasRequestPending) {
-  //             console.log('updated from cache');
-  //             json.key = key;
-  //             json.label = label;
-  //             app.updateForecastCard(json);
-  //           }
-  //         });
-  //       }
-  //     });
-  //   }
-  //   // Make the XHR to get the data, then update the card
-  //   app.hasRequestPending = true;
-  //   var request = new XMLHttpRequest();
-  //   request.onreadystatechange = function () {
-  //     if (request.readyState === XMLHttpRequest.DONE) {
-  //       if (request.status === 200) {
-  //         var response = JSON.parse(request.response);
-  //         response.key = key;
-  //         response.label = label;
-  //         app.hasRequestPending = false;
-  //         app.updateForecastCard(response);
-  //       }
-  //     }
-  //   };
-  //   request.open('GET', url);
-  //   request.send();
-  // };
-
-  // // Iterate all of the cards and attempt to get the latest forecast data
-  // app.updateForecasts = function () {
-  //   var keys = Object.keys(app.visibleCards);
-  //   keys.forEach(function (key) {
-  //     app.getForecast(key);
-  //   });
-  // };
-
-  // // Save list of cities to localStorage, see note below about localStorage.
-  // app.saveSelectedCities = function () {
-  //   var selectedCities = JSON.stringify(app.selectedCities);
-  //   // IMPORTANT: See notes about use of localStorage.
-  //   localStorage.selectedCities = selectedCities;
-  // };
-
-  // app.selectedCities = localStorage.selectedCities;
-  // if (app.selectedCities) {
-  //   app.selectedCities = JSON.parse(app.selectedCities);
-  //   app.selectedCities.forEach(function (city) {
-  //     app.getForecast(city.key, city.label);
-  //   });
-  // } else {
-  //   app.updateForecastCard(initialWeatherForecast);
-  //   app.selectedCities = [{
-  //     key: initialWeatherForecast.key,
-  //     label: initialWeatherForecast.label
-  //   }];
-  //   app.saveSelectedCities();
   // }
 
 })(window, document, location, idbKeyval);
